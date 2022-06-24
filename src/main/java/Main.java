4cdd5963158f4dfe6402c861ff077bf8b74b4fa9
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-06-24 14:07:28 -0400

    added a break

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main {
                 System.out.println("Too high. Try again");
             } else if (guess == number) {
                 System.out.println("Ding ding ding. Congratulations");
+                break;
             }
             }
         }
