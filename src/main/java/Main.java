3f968a199d22c74a9e203770d1b5bc785176aad8
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-06-24 13:35:42 -0400

    completed project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int number = (int)(Math.random()*100 + 1);
+        System.out.println("Guess a number between 1 and 100");
+        int guess = ' ';
+        while (guess != number) {
+            guess = scan.nextInt();
+            if (guess < number) {
+                System.out.println("Too low. Try again");
+            } else if (guess > number) {
+                System.out.println("Too high. Try again");
+            } else if (guess == number) {
+                System.out.println("Ding ding ding. Congratulations");
+            }
+            }
+        }
         
     }
-}
+
